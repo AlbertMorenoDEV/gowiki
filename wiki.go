aea55fa77e67f592a112653f0b1bb6606cb7b33c
AlbertMorenoDEV <albert.moreno.dev@gmail.com>
2017-08-24 22:58:16 +0200

    Front page redirect

--- wiki.go
+++ wiki.go
@@ -32,11 +32,16 @@ func loadPage(title string) (*Page, error) {
 }
 
 func main() {
+	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/view/FrontPage", http.StatusFound)
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
